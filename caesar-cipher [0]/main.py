c440f6fa594dd32850cd9ccdf0c582d0ce9164b0
Henawy Programmer <111087686+AhmedHenawy11@users.noreply.github.com>
2023-09-11 04:47:58 +0200

    Update main.py

--- caesar-cipher [0]/main.py
+++ caesar-cipher [0]/main.py
@@ -1,13 +1,17 @@
 from art import logo
 print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 # STEP 1 Making inputs
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 if shift > 26:
   shift = shift % 26
+  
 #STEP 2 encoding function
+
 def encoding_process( direction, text, shift):
   crypto = ""
   if direction == "encode":
@@ -27,7 +31,9 @@ def encoding_process( direction, text, shift):
         crypto += letter
     print(f"The decoded text is: {crypto}")
 encoding_process(direction, text, shift )
+
 #STEP 3 continuis process
+
 choice = input("Type 'yes' if you want to go again, Otherwise type 'no'\n")
 while choice == 'yes':
   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
